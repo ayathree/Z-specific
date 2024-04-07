f46e905453b7e205ce710e253b9632513ffcb219
ayathree <nobanitaayathree333@gmail.com>
2024-04-08 03:51:06 +0600

    make API json file

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -2,9 +2,12 @@ import {Outlet} from 'react-router-dom'
 import Footer from '../components/Footer';
 import Nav from '../components/Nav';
 
+
 const Root = () => {
+    
     return (
         <div className='container mx-auto lg:px-12 py-8 px-3'>
+           
             <Nav></Nav>
            <Outlet></Outlet> 
            <Footer></Footer>
