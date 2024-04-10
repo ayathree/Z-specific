220cc0ccd2796ce9f7d86daf20858432f93bc5b8
ayathree <nobanitaayathree333@gmail.com>
2024-04-10 14:34:44 +0600

    make the cardDetails page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,12 +5,12 @@ const Card = ({card}) => {
     const{image,estate_title,id}=card
     return (
         <div>
-            <div  className="card card-compact border-white border-4 bg-gradient-to-r from-black to-gray-300 ">
-            <figure><img className="h-[250px] w-[500px]" src={image} alt="" /></figure>
-            <div className="card-body">
-              <h2 className="card-title text-white text-3xl">{estate_title}</h2>
+            <div  className="card card-compact  p-11 ">
+            <figure><img className="h-[250px] w-[500px] border-4 border-white rounded-3xl" src={image} alt="" /></figure>
+            <div className="card-body ">
+              <h2 className="card-title  text-white text-xl">{estate_title}</h2>
               
-              <div className="card-actions justify-end">
+              <div className="card-actions justify-center">
                 <Link to={`/card/${id}`}><button className="btn bg-white border-red-600 border-4 text-red-600">View Property</button></Link>
               </div>
             </div>
--- src/pages/CardDetails.jsx
+++ src/pages/CardDetails.jsx
@@ -1,4 +1,7 @@
 import { useLoaderData,useParams} from 'react-router-dom'
+import { MdOutlinePriceChange,  } from "react-icons/md";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaChartArea } from "react-icons/fa";
 
 const CardDetails = () => {
     
@@ -11,7 +14,55 @@ const CardDetails = () => {
     
     return (
         <div>
-            <h2 className='text-white'>{card.segment_name}</h2>
+            
+      <h1 className="lg:text-5xl font-bold text-white text-center ">{card.estate_title}</h1>
+            <div className="hero min-h-scree">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <img  src={card.image} className=" rounded-lg border-4 lg:max-w-lg border-white" />
+    <div>
+      
+        <div className='flex flex-row gap-3  item-center mt-6'>
+            <p className='lg:text-2xl text-white font-bold underline underline-offset-4'>Category:</p>
+            <p className='text-white lg:text-2xl'>{card.segment_name}</p>
+        </div>
+        <div className='flex flex-row items-center mt-6 gap-9'>
+            <div className='flex flex-row items-center gap-3'>
+                <p className='text-white'><MdOutlinePriceChange /></p>
+                <p className='text-white'>{card.price}</p>
+            </div>
+            <div className='flex flex-row items-center gap-3' >
+                <p className='text-white underline underline-offset-4'>Status:</p>
+                <p className='text-white'>{card.status}</p>
+            </div>
+        </div>
+        <div className='mt-6'>
+            <p className='text-white'><span className='lg:text-2xl font-bold underline underline-offset-4'>Description:</span> {card.description}</p>
+        </div>
+        <div className='flex flex-row items-center mt-6 gap-9'>
+            <div className='flex flex-row items-center gap-3'>
+                <p className='text-white'><FaChartArea /></p>
+                <p className='text-white'>{card.area}</p>
+            </div>
+            <div className='flex flex-row items-center gap-3' >
+                <p className='text-white '><FaLocationDot /></p>
+                <p className='text-white'>{card.location}</p>
+            </div>
+        </div>
+        <div className=' mt-6' >
+                <p className='text-white text-center lg:text-start md:text-start underline underline-offset-4 lg:text-2xl font-bold'>Facilities:</p>
+                <div className='flex lg:flex-row md:flex-row flex-col items-center lg:gap-3 md:gap-3'>
+                <p className='text-white'>{card.facilities[0]},</p>
+                <p className='text-white'>{card.facilities[1]},</p>
+                <p className='text-white'>{card.facilities[2]}</p>
+                </div>
+            </div>
+        
+
+     
+     
+    </div>
+  </div>
+</div>
             
            
         </div>
